b7c04415b29a2f136944c0103a9b376031e1016e
Tomsz Kawczyński <tomasz.kawczynski@gmail.com>
2018-03-14 11:08:30 +0100

    fix attempt for unexpected restarts of cassandra db

--- spark-shell.sh
+++ spark-shell.sh
@@ -3,4 +3,5 @@ DOCKER_NETWORK=`docker network list | grep cassandra-spark | awk '{print $2}'`
 
 docker run --network ${DOCKER_NETWORK} -it -p4040:4040 --rm spark-shell \
 	--master spark://spark-master:7077 \
-	--packages datastax:spark-cassandra-connector:2.0.7-s_2.11 $@
+	--packages datastax:spark-cassandra-connector:2.0.7-s_2.11 \
+	--conf spark.cassandra.connection.host=cassandra-1 $@
