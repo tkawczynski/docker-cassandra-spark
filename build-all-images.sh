0dd265202318ba3c59d90b6e4f7de80f64b01323
Tomasz Kawczyński <tomasz.kawczynski@gmail.com>
2018-08-17 09:34:53 -0400

    added Zeppelin to the mix

--- build-all-images.sh
+++ build-all-images.sh
@@ -7,6 +7,7 @@ if  hash docker 2>/dev/null; then
 	docker build -t spark-master $SCRIPT_DIR/spark-master
 	docker build -t cassandra-spark-worker $SCRIPT_DIR/cassandra-spark-worker
 	docker build -t spark-shell $SCRIPT_DIR/spark-shell
+	docker build -t spark-zeppelin $SCRIPT_DIR/spark-zeppelin
 else 
 	echo "Docker not installed"
 	exit 1
